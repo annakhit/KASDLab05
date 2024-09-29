21708f68187e88653f511249e6e4f676fa5c2287
Anna Khit <annakhit02@mail.ru>
2024-09-29 15:05:47 +0300

    Implemented FileReader

---
+++ FileReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+public static class FileReader
+{
+    public static T[] ReadOneDimensionalArray<T>(string filePath, Func<string, T> parser)
+    {
+        string[] lines = File.ReadAllLines(filePath);
+        return lines.Select(parser).ToArray();
+    }
+}
