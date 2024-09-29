b685992eb6cf9dccaaa743adca26b30d51d99b64
Anna Khit <annakhit02@mail.ru>
2024-09-29 20:15:15 +0300

    Implemented HTMLReader

---
+++ HTMLParser.cs
@@ -0,0 +1,20 @@
+﻿public class HTMLParser
+{
+    readonly MyArrayList<string> tagsArray = new MyArrayList<string>();
+    public MyArrayList<string> Parse(string[] input)
+    {
+        foreach (string item in input)
+        {
+            foreach (string element in item.Split('<'))
+            {
+                if (element.IndexOf('>', 0) != -1)
+                {
+                    string tag = element.ToLower().Substring(0, element.IndexOf('>', 0)).Replace("/", "");
+                    if (!tagsArray.Contains(tag)) tagsArray.AddElement(tag);
+                }
+            }
+        }
+
+        return tagsArray;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KASDLab05
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string[] data = FileReader.ReadOneDimensionalArray("../../Data/input.txt", value => value);
+
+            HTMLParser parser = new HTMLParser();
+
+            MyArrayList<string> result = parser.Parse(data);
+
+            result.Print();
+
+            Console.ReadKey();
         }
     }
 }
